699b05758b2057598d1127be9e8ce6f0c6679145
joelsipayung <44727133+joelsipayung@users.noreply.github.com>
2018-12-03 15:22:35 +0700

    Add files via upload

---
+++ src/resumeData.js
@@ -0,0 +1,96 @@
+let resumeData = {
+  "imagebaseurl":"https://techonoeticsclub.github.io/",
+  "name": "Joel Verdy Stevan Sipayung",
+  "role": "Web programmer and a Network engineer",
+  // "linkedinId":"Your LinkedIn Id",
+  "phone":" 081230291607",
+  "skypeid": "Your skypeid",
+  "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, listen music or simply play games in my free time.",
+  "socialLinks":[
+      {
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/in/joel-verdy-stevan-sipayung-a98184132//",
+        "className":"fa fa-linkedin"
+      },
+      {
+        "name":"github",
+        "url":"https://github.com/joelsipayung",
+        "className":"fa fa-github"
+      },
+      {
+        "name":"skype",
+        "url":"http://skype.com",
+        "className":"fa fa-skype"
+      }
+    ],
+  "aboutme":"I am a computer engineer student. Have experience about web programming, network and cloud computing when i studied in my University. And also i have been internship in a Company to development website",
+  "address":"Sibatu-batu street, Pematangsiantar, North Sumatra, 21139",
+  "email":"jverdysipayung@gmail.com",
+  "education":[
+    {
+      "UniversityName":"Del Institute of Technology",
+      // "specialization":"Some specialization",
+      "MonthOfPassing":"2016 - ",
+      "YearOfPassing":"2019",
+      // "Achievements":"Some Achievements"
+    }
+  ],
+  "work":[
+    {
+      "CompanyName":"PT. Dutakalingga Pratama",
+      "specialization":"Web Development",
+      "MonthOfLeaving":"July - September",
+      "YearOfLeaving":"2018",
+      // "Achievements":"Some Achievements"
+    }
+  ],
+  "skillsDescription":"This is my skills",
+  "skills":[
+    {
+      "skillname":"HTML5"
+    },
+    {
+      "skillname":"CSS"
+    },
+    {
+      "skillname":"PHP"
+    },
+    {
+      "skillname":"Reactjs"
+    }
+  ],
+  "portfolio":[
+    {
+      "name":"project1",
+      "description":"miniproject",
+      "imgurl":"images/portfolio/phone.jpg"
+    },
+    {
+      "name":"project2",
+      "description":"lumbanbulbul",
+      "imgurl":"images/portfolio/project.jpg"
+    },
+    {
+      "name":"project3",
+      "description":"sistemizin",
+      "imgurl":"images/portfolio/project2.png"
+    },
+    {
+      "name":"project4",
+      "description":"magang",
+      "imgurl":"images/portfolio/phone.jpg"
+    }
+  ],
+  "testimonials":[
+    {
+      "description":"produkmu nya bagus",
+      "name":"joel"
+    },
+    {
+      "description":"Awesome",
+      "name":"Verdy"
+    }
+  ]
+}
+
+export default resumeData
